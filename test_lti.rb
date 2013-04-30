927ebce6c0f6fe5cfc629ffc36ceada4442a0f0d
Brian Whitmer <brian@instructure.com>
2013-04-30 10:24:53 -0600

    allow _method calls in test API

--- test_lti.rb
+++ test_lti.rb
@@ -31,9 +31,11 @@ class TestLti < Sinatra::Base
   # sinatra wants to set x-frame-options by default, disable it
   disable :protection
   
+  
   enable :sessions
   raise "session key required" if ENV['RACK_ENV'] == 'production' && !ENV['SESSION_KEY']
   set :session_secret, ENV['SESSION_KEY'] || "local_secret"
+  set :method_override, true
   
   env = ENV['RACK_ENV'] || settings.environment
   DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/#{env}.sqlite3"))
